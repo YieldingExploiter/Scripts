3b5eb0ef71fe5a2670cd7d3616dec22f8a92d3b5
YieldingExploiter <90570076+YieldingExploiter@users.noreply.github.com>
2021-10-23 15:11:13 +0200

    Update Script.lua

--- src/BadBuisness/Script.lua
+++ src/BadBuisness/Script.lua
@@ -1,252 +1,139 @@
--- See https://github.com/YieldingExploiter/Scripts/blob/main/src/BadBuisness/Script.lua
-return (function()
-  local client = game.Players.LocalPlayer
-  local camera = workspace.CurrentCamera
-  local mouse = client:GetMouse()
-  local UIS = game:GetService('UserInputService')
-  local rs = game:GetService('RunService')
+-- This script only contains the UI portions and a loader for the Bad Buisness Script.
+print(
+  [[
+  ---- Bad Buisness 2.48
+  | Original Script   | https://v3rmillion.net/showthread.php?tid=1141282                                   |
 
-  local function myCharacter()
-    for i, v in ipairs(workspace.Characters:GetChildren()) do
-      if (v.Body.Head.Position - camera.CFrame.Position).magnitude < 1 then return v end
-    end
-  end
+  Credits
+  | Original Script   | Throit                                                                              |
+  | UI                | YieldingCoder                                                                       |
+  
+  Source Code
+  | Script            | https://github.com/YieldingExploiter/Scripts/blob/main/src/BadBuisness/Script.lua   |
+  | Discord UI        | https://github.com/YieldingExploiter/Scripts/tree/main/src/ui/dlib.lua              |
+]]
+)
 
-  local function onTeam( x )
-    for i, v in ipairs(client.PlayerGui:GetChildren()) do
-      if v.Name == 'NameGui' then if v.Adornee:IsDescendantOf(x) then return true end end
-    end
-  end
+local versionInfo = {
+  ['Branch'] = 'Stable';
+  ['Version'] = '0.1.0';
+  ['Build'] = '00001';
+  ['Hash'] = '5c8edf2'; -- Last Git hash seen on https://github.com/YieldingExploiter/Scripts/tree/main/src/BadBuisness at the time of last source modification.
+  ['LastTested'] = 1139; -- Last version the script was tested on
+}
 
-  -- ANCHOR Aimbot
-  local function closestPlayer( fov )
-    local closest = fov
-    local target = nil
-    for i, v in ipairs(workspace.Characters:GetChildren()) do
-      if v ~= myCharacter() and myCharacter() and not onTeam(v) then
-        local _, onscreen = camera:WorldToScreenPoint(v.Body.Head.Position)
-        if onscreen then
-          local targetPos = camera:WorldToViewportPoint(v.Body.Head.Position)
-          local mousePos = camera:WorldToViewportPoint(mouse.Hit.p)
-          local dist = (Vector2.new(mousePos.X, mousePos.Y) -
-                         Vector2.new(targetPos.X, targetPos.Y)).magnitude
-          if dist < closest then
-            closest = dist
-            target = v
-          end
-        end
-      end
-    end
-    return target
-  end
-  local function aimAt( pos, smooth )
-    local targetPos = camera:WorldToScreenPoint(pos)
-    local mousePos = camera:WorldToScreenPoint(mouse.Hit.Position)
-    mousemoverel((targetPos.X - mousePos.X) / smooth, (targetPos.Y - mousePos.Y) / smooth)
-  end
-  local isAiming = false
-  mouse.Button2Down:Connect(function() isAiming = true end)
-  mouse.Button2Up:Connect(function() isAiming = false end)
-
-  -- ANCHOR Silent Aim 
-  local silentFovObject = Drawing.new('Circle')
-  silentFovObject.Radius = _G.BBconfig.fovAmount
-  silentFovObject.Filled = true
-  silentFovObject.Transparency = _G.BBconfig.fovTransparency or 0.7
-  silentFovObject.Position = Vector2.new(0, 0)
-  silentFovObject.Visible = true
-  silentFovObject.Color = _G.BBconfig.fovColor
-  local silentHead = Drawing.new('Circle')
-  silentHead.Radius = 10
-  silentHead.Filled = true
-  silentHead.Transparency = 0.4
-  silentHead.Position = Vector2.new(0, 0)
-  silentHead.Visible = false
-  silentHead.Color = Color3.fromRGB(255, 0, 0)
-  local isShooting = false
-
-  local silentAimEnabled = true
-  local gmt = getrawmetatable(game)
-  setreadonly(gmt, false)
-  local oldNamecall = gmt.__namecall
-  gmt.__namecall = newcclosure(
-    function( self, ... )
-      local Args = { ... }
-      local method = getnamecallmethod()
-      if tostring(self) == 'Projectiles' and tostring(method) == 'FireServer' and
-        closestPlayer(_G.BBconfig.fovAmount) and silentAimEnabled then
-        Args[3] = closestPlayer(_G.BBconfig.fovAmount).Hitbox.Head
-        Args[4] = closestPlayer(_G.BBconfig.fovAmount).Hitbox.Head.Position
-        return self.FireServer(self, unpack(Args))
-      end
-      return oldNamecall(self, ...)
-    end
+local SettingsLib = loadstring(
+  game:HttpGetAsync(
+    'https://raw.githubusercontent.com/YieldingExploiter/Scripts/main/dist/glib/settings/Script.lua',
+    true
   )
-  -- ANCHOR Fly
-  local velPart = Instance.new('BodyVelocity')
-  local flySettings = {
-    speed = _G.BBconfig.flySpeed;
-    enabled = false;
-    ['mf'] = {
-      [true] = Vector3.new(math.huge, math.huge, math.huge);
-      [false] = Vector3.new();
-    };
-    multCF = CFrame.new(0, 0, 0);
-  }
-  UIS.InputBegan:Connect(
-    function( input )
-      local newCF = flySettings.multCF
-      if input.KeyCode == Enum.KeyCode.F then
-        flySettings.enabled = not flySettings.enabled
-        velPart.MaxForce = flySettings['mf'][flySettings.enabled]
-      elseif input.KeyCode == Enum.KeyCode.W then
-        newCF = CFrame.new(newCF.X, newCF.Y, -5)
-      elseif input.KeyCode == Enum.KeyCode.S then
-        newCF = CFrame.new(newCF.X, newCF.Y, 5)
-      elseif input.KeyCode == Enum.KeyCode.A then
-        newCF = CFrame.new(-5, newCF.Y, newCF.Z)
-      elseif input.KeyCode == Enum.KeyCode.D then
-        newCF = CFrame.new(5, newCF.Y, newCF.Z)
-      elseif input.KeyCode == Enum.KeyCode.Space then
-        newCF = CFrame.new(newCF.X, 5, newCF.Z)
-      elseif input.KeyCode == Enum.KeyCode.LeftShift then
-        newCF = CFrame.new(newCF.X, -5, newCF.Z)
-      elseif input.UserInputType == Enum.UserInputType.MouseButton1 then
-        isShooting = true
-      end
-      flySettings.multCF = newCF
-    end
-  )
-  UIS.InputEnded:Connect(
-    function( input )
-      local newCF = flySettings.multCF
-      if input.KeyCode == Enum.KeyCode.W and newCF.Z == -5 then
-        newCF = CFrame.new(newCF.X, newCF.Y, 0)
-      elseif input.KeyCode == Enum.KeyCode.S and newCF.Z == 5 then
-        newCF = CFrame.new(newCF.X, newCF.Y, 0)
-      elseif input.KeyCode == Enum.KeyCode.A and newCF.X == -5 then
-        newCF = CFrame.new(0, newCF.Y, newCF.Z)
-      elseif input.KeyCode == Enum.KeyCode.D and newCF.X == 5 then
-        newCF = CFrame.new(0, newCF.Y, newCF.Z)
-      elseif input.KeyCode == Enum.KeyCode.Space and newCF.Y == 5 then
-        newCF = CFrame.new(newCF.X, 0, newCF.Z)
-      elseif input.KeyCode == Enum.KeyCode.LeftShift and newCF.Y == -5 then
-        newCF = CFrame.new(newCF.X, 0, newCF.Z)
-      elseif input.UserInputType == Enum.UserInputType.MouseButton1 then
-        isShooting = false
-      end
-      flySettings.multCF = newCF
-    end
-  )
-  velPart.MaxForce = Vector3.new(0, 0, 0)
+)()
+local Settings = SettingsLib.new('BadBuisness')
 
-  -- ANCHOR ESP
-  local boxes = {}
-  local function newLine()
-    local x = Drawing.new('Line')
-    x.To = Vector2.new(2, 2)
-    x.From = Vector2.new(0, 0)
-    x.Visible = false
-    x.Thickness = 2
-    x.Color = _G.BBconfig.espColor
-    return x
-  end
+local tableToColor3 = function( inTable )
+  return Color3.fromRGB(inTable.R, inTable.G, inTable.B)
+end
+local color3ToTable = function( inClr3 )
+  return { ['R'] = inClr3.R * 255; ['G'] = inClr3.G * 255; ['B'] = inClr3.B * 255 }
+end
 
-  local function newBox( x )
-    local box = { ['Character'] = x; newLine(); newLine(); newLine(); newLine() }
-    table.insert(boxes, box)
-  end
+_G.BBconfig = {
+  -- ANCHOR espColor = BBConfig.ESP.Clr
+  espColor = tableToColor3(
+    Settings:Default(
+      'BBConfig.ESP.Clr', color3ToTable(Color3.new(1, 0, 0))
+    ):Get('BBConfig.ESP.Clr')
+  );
+  -- ANCHOR fovColor = BBConfig.FOV.Clr
+  fovColor = tableToColor3(
+    Settings:Default(
+      'BBConfig.FOV.Clr', color3ToTable(Color3.fromRGB(255, 255, 255))
+    ):Get('BBConfig.FOV.Clr')
+  );
+  fovTransparency = Settings:Default('BBConfig.FOV.Transparency', 0.7):Get(
+    'BBConfig.FOV.Transparency'
+  );
+  -- ANCHOR fovAmount = BBConfig.FOV.Amnt
+  fovAmount = Settings:Default('BBConfig.FOV.Amnt', 300):Get('BBConfig.FOV.Amnt');
+  -- ANCHOR aimbotSmoothness = BBConfig.Aimbot.Smoothness
+  aimbotSmoothness = Settings:Default('BBConfig.Aimbot.Smoothness', 2):Get(
+    'BBConfig.Aimbot.Smoothness'
+  );
+  -- ANCHOR flySpeed = BBConfig.Fly.Speed
+  flySpeed = Settings:Default('BBConfig.Fly.Speed', 90):Get('BBConfig.Fly.Speed');
+}
 
-  workspace.Characters.ChildAdded:Connect(
-    function( x ) if not onTeam(x) and x ~= myCharacter() then newBox(x) end end
+local LoadInternal = loadstring(
+  game:HttpGetAsync(
+    -- TODO: Minify and use dist dir
+    ('https://github.com/YieldingExploiter/Scripts/blob/main/src/BadBuisness/Internal.lua?raw=true'),
+    true
   )
-  for i, x in ipairs(workspace.Characters:GetChildren()) do
-    if not onTeam(x) and x ~= myCharacter() then newBox(x) end
-  end
-  local function updateEsp()
-    for i, v in ipairs(boxes) do
-      if not v['Character']:IsDescendantOf(workspace) then
-        for i, x in ipairs(v) do x.Remove() end
-        table.remove(boxes, i)
-      else
-        for i, x in ipairs(v) do x.Color = _G.BBconfig.espColor end
-        local character = v['Character']
-        local TL = camera:WorldToViewportPoint(
-          (character.Root.CFrame * CFrame.new(-2, 3, 0)).p
-        )
-        local TR = camera:WorldToViewportPoint(
-          (character.Root.CFrame * CFrame.new(2, 3, 0)).p
-        )
-        local BL = camera:WorldToViewportPoint(
-          (character.Root.CFrame * CFrame.new(-2, -5, 0)).p
-        )
-        local BR = camera:WorldToViewportPoint(
-          (character.Root.CFrame * CFrame.new(2, -5, 0)).p
-        )
-        v[1].From = Vector2.new(TL.X, TL.Y)
-        v[1].To = Vector2.new(TR.X, TR.Y)
-        v[2].From = Vector2.new(TR.X, TR.Y)
-        v[2].To = Vector2.new(BR.X, BR.Y)
-        v[3].From = Vector2.new(BR.X, BR.Y)
-        v[3].To = Vector2.new(BL.X, BL.Y)
-        v[4].From = Vector2.new(BL.X, BL.Y)
-        v[4].To = Vector2.new(TL.X, TL.Y)
-        local _, onscreen = camera:WorldToScreenPoint(character.Root.Position)
-        if onTeam(v['Character']) then onscreen = false end
-        for i, x in ipairs(v) do x.Visible = onscreen end
-      end
-    end
-  end
-  updateEsp();
+)
 
-  -- SECTION Main Loop
-  rs.RenderStepped:Connect(
-    function()
-      -- ANCHOR ESP 
-      updateEsp()
-      -- ANCHOR Fly
-      flySettings.speed = _G.BBconfig.flySpeed
-      if myCharacter() then
-        local character = myCharacter()
-        if not velPart:IsDescendantOf(character) then
-          velPart.Parent = character.Root
-        end
-        local old = flySettings.multCF
-        local newCF = character.Root.CFrame * CFrame.new(old.X, old.Y, old.Z)
-        local newPos = newCF.p
-        if newPos ~= character.Root.Position and newPos then
-          character.Root.Anchored = false
-          velPart.Velocity = CFrame.new(character.Root.Position, newPos).LookVector *
-                               flySettings.speed
-        elseif flySettings.enabled then
-          character.Root.Anchored = true
-        else
-          character.Root.Anchored = false
-        end
-      end
-      -- ANCHOR Aimbot
-      local mousePos = camera:WorldToViewportPoint(mouse.Hit.p)
-      silentFovObject.Position = Vector2.new(mousePos.X, mousePos.Y)
-      if isAiming and closestPlayer(_G.BBconfig.fovAmount) then
-        aimAt(
-          closestPlayer(_G.BBconfig.fovAmount).Hitbox.Head.Position,
-          _G.BBconfig.aimbotSmoothness
-        )
-      end
-      -- ANCHOR Silent Aim 
-      silentFovObject.Color = _G.BBconfig.fovColor -- Live Update Config
-      silentFovObject.Transparency = _G.BBconfig.fovTransparency or 0.7 -- Live Update Config
-      silentFovObject.Radius = _G.BBconfig.fovAmount or 300 -- Live Update Config
-      if isShooting and closestPlayer(_G.BBconfig.fovAmount) then
-        local pos = camera:WorldToViewportPoint(
-          closestPlayer(_G.BBconfig.fovAmount).Hitbox.Head.Position
-        )
-        silentHead.Position = Vector2.new(pos.X, pos.Y)
-        silentHead.Visible = true
-      else
-        silentHead.Visible = false
-      end
-    end
+local DiscordLib = loadstring(
+  game:HttpGetAsync(
+    'https://raw.githubusercontent.com/YieldingExploiter/Scripts/main/dist/ui/dlib.lua',
+    true
   )
-  -- !SECTION
-end)()
+)()
+
+local win = DiscordLib:Window('Discock', versionInfo)
+if game.PlaceId ~= 3233893879 then return end
+local Server = win:Server('Bad Buisness Script', '')
+if game.PlaceVersion > versionInfo.LastTested then
+  local Outdated = Server:Channel('Outdated Script')
+  Outdated:Label(
+    'This script was last tested on Game Version ' .. tostring(versionInfo.LastTested)
+  )
+  Outdated:Label('The current Game Version is ' .. tostring(game.PlaceVersion))
+  Outdated:Label('As you can tell, this is not that cool and sex')
+  Outdated:Seperator();
+  Outdated:Label('tl;dr:')
+  Outdated:Label('the script might not work and might be detected')
+end
+
+local ESP = Server:Channel('ESP')
+ESP:Colorpicker(
+  'ESP Color', tableToColor3(Settings:Get('BBConfig.ESP.Clr')), function( new )
+    Settings:Set('BBConfig.ESP.Clr', color3ToTable(new))
+    _G.BBconfig.espColor = new
+  end
+)
+local FOV = Server:Channel('FOV')
+FOV:Colorpicker(
+  'FOV Color', tableToColor3(Settings:Get('BBConfig.FOV.Clr')), function( new )
+    Settings:Set('BBConfig.FOV.Clr', color3ToTable(new))
+    _G.BBconfig.fovColor = new
+  end
+)
+FOV:Slider(
+  'FOV Opacity', 0, 800, Settings:Get('BBConfig.FOV.Transparency') * 1000, function( new )
+    new = new / 1000
+    Settings:Set('BBConfig.FOV.Transparency', new)
+    _G.BBconfig.fovTransparency = new
+  end
+)
+FOV:Slider(
+  'FOV Amount', 0, 1000, Settings:Get('BBConfig.FOV.Amnt'), function( new )
+    Settings:Set('BBConfig.FOV.Amnt', new)
+    _G.BBconfig.fovAmount = new
+  end
+)
+local Aimbot = Server:Channel('Aimbot')
+Aimbot:Slider(
+  'Smoothness', 0, 10, Settings:Get('BBConfig.Aimbot.Smoothness'), function( new )
+    Settings:Set('BBConfig.Aimbot.Smoothness', new)
+    _G.BBconfig.aimbotSmoothness = new
+  end
+)
+local Fly = Server:Channel('Fly')
+Fly:Slider(
+  'Speed', 0, 250, Settings:Get('BBConfig.Fly.Speed'), function( new )
+    Settings:Set('BBConfig.Fly.Speed', new)
+    _G.BBconfig.flySpeed = new
+  end
+)
+Fly:Label('Fly Keybind is F')
+Fly:Label('(might add rebinding later)')
+LoadInternal()()
