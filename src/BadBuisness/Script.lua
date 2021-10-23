01e357eeb263a07236908bdca7db5df2b71d9bbb
YieldingExploiter <90570076+YieldingExploiter@users.noreply.github.com>
2021-10-23 02:22:56 +0200

    Update Script.lua

--- src/BadBuisness/Script.lua
+++ src/BadBuisness/Script.lua
@@ -14,6 +14,14 @@ print(
 ]]
 )
 
+local versionInfo = {
+  ['Branch'] = 'Stable';
+  ['Version'] = '0.1.0';
+  ['Build'] = '00001';
+  ['Hash'] = '4d7c418'; -- Last Git hash seen on https://github.com/YieldingExploiter/Scripts/tree/main/src/BadBuisness at the time of last source modification.
+  ['LastTested'] = 1138; -- Last version the script was tested on
+}
+
 local SettingsLib = loadstring(
   game:HttpGetAsync(
     'https://raw.githubusercontent.com/YieldingExploiter/Scripts/main/dist/glib/settings/Script.lua',
@@ -22,4 +30,89 @@ local SettingsLib = loadstring(
 )()
 local Settings = SettingsLib.new('BadBuisness')
 
-loadstring(game:HttpGetAsync(('https://raw.githubusercontent.com/Straden/Scripts/main/BadBuisness.lua'),true))()
+local tableToColor3 = function( inTable )
+  return Color3.fromRGB(inTable.R, inTable.G, inTable.B)
+end
+local color3ToTable = function( inClr3 )
+  return { ['R'] = inClr3.R * 255; ['G'] = inClr3.G * 255; ['B'] = inClr3.B * 255 }
+end
+
+_G.BBconfig = {
+  -- ANCHOR espColor = BBConfig.ESP.Clr
+  espColor = tableToColor3(
+    Settings:Default(
+      'BBConfig.ESP.Clr', color3ToTable(Color3.new(1, 0, 0))
+    ):Get('BBConfig.ESP.Clr')
+  );
+  -- ANCHOR fovColor = BBConfig.FOV.Clr
+  fovColor = tableToColor3(
+    Settings:Default(
+      'BBConfig.FOV.Clr', color3ToTable(Color3.fromRGB(255, 255, 255))
+    ):Get('BBConfig.FOV.Clr')
+  );
+  fovTransparency = Settings:Default('BBConfig.FOV.Transparency', 0.7):Get(
+    'BBConfig.FOV.Transparency'
+  );
+  -- ANCHOR fovAmount = BBConfig.FOV.Amnt
+  fovAmount = Settings:Default('BBConfig.FOV.Amnt', 300):Get('BBConfig.FOV.Amnt');
+  -- ANCHOR aimbotSmoothness = BBConfig.Aimbot.Smoothness
+  aimbotSmoothness = Settings:Default('BBConfig.Aimbot.Smoothness', 2):Get(
+    'BBConfig.Aimbot.Smoothness'
+  );
+  -- ANCHOR flySpeed = BBConfig.Fly.Speed
+  flySpeed = Settings:Default('BBConfig.Fly.Speed', 90):Get('BBConfig.Fly.Speed');
+}
+
+local LoadInternal = loadstring(
+  game:HttpGetAsync(
+    -- TODO: Minify and use dist dir
+    ('https://github.com/YieldingExploiter/Scripts/blob/main/src/BadBuisness/Internal.lua?raw=true'),
+    true
+  )
+)()
+LoadInternal()
+
+local DiscordLib = loadstring(
+  game:HttpGetAsync(
+    'https://raw.githubusercontent.com/YieldingExploiter/Scripts/main/dist/ui/dlib.lua',
+    true
+  )
+)()
+
+local win = DiscordLib:Window('Discock', versionInfo)
+if game.PlaceId ~= 3233893879 then return end
+local Server = win:Server('Bad Buisness Script', '')
+if game.PlaceVersion > versionInfo.LastTested then
+  local Outdated = Server:Channel('Outdated Script')
+  Outdated:Label(
+    'This script was last tested on Game Version ' .. tostring(versionInfo.LastTested)
+  )
+  Outdated:Label('The current Game Version is ' .. tostring(game.PlaceVersion))
+  Outdated:Label('As you can tell, this is not that cool and sex')
+  Outdated:Seperator();
+  Outdated:Label('tl;dr:')
+  Outdated:Label('the script might not work and might be detected')
+end
+
+local ESP = Server:Channel('ESP')
+ESP:Colorpicker(
+  'ESP Color', tableToColor3(Settings:Get('BBConfig.ESP.Clr')), function( new )
+    Settings:Set('BBConfig.ESP.Clr', color3ToTable(new))
+    _G.BBconfig.espColor = new
+  end
+)
+local FOV = Server:Channel('FOV')
+FOV:Colorpicker(
+  'FOV Color', tableToColor3(Settings:Get('BBConfig.FOV.Clr')), function( new )
+    Settings:Set('BBConfig.FOV.Clr', color3ToTable(new))
+    _G.BBconfig.fovColor = new
+  end
+)
+FOV:Slider(
+  'FOV Transparency', 0, 1000, Settings:Get('BBConfig.FOV.Transparency') * 1000,
+  function( new )
+    new = new / 1000
+    Settings:Set('BBConfig.FOV.Transparency', new)
+    _G.BBconfig.fovTransparency = new
+  end
+)
