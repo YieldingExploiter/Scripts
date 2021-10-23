8e74e463a73c76beda683c7c000ef752e5ff259b
YieldingExploiter <90570076+YieldingExploiter@users.noreply.github.com>
2021-10-23 02:34:39 +0200

    Create Loader.lua

---
+++ src/BadBuisness/Loader.lua
@@ -0,0 +1,13 @@
+--[[
+  ---- Bad Buisness 2.48
+  | Original Script   | https://v3rmillion.net/showthread.php?tid=1141282                                   |
+  Credits
+  | Original Script   | Throit                                                                              |
+  | UI                | YieldingCoder                                                                       |
+  
+  Source Code
+  | Script            | https://github.com/YieldingExploiter/Scripts/blob/main/src/BadBuisness/Script.lua   |
+  | Discord UI        | https://github.com/YieldingExploiter/Scripts/tree/main/src/ui/dlib.lua              |
+]]
+
+loadstring(game:HttpGetAsync('https://github.com/YieldingExploiter/Scripts/blob/main/src/BadBuisness/Script.lua'))()
